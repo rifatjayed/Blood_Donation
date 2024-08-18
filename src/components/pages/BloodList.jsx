34b2d23714c192548568c7d70291c5dd1f89663e
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-08-18 23:14:23 +0600

    blood List Table responsive done

--- src/components/pages/BloodList.jsx
+++ src/components/pages/BloodList.jsx
@@ -12,13 +12,31 @@ const BloodList = () => {
       bloodGroup: "A+",
       lastDonationDate: "10-12-24",
     },
+    {
+      id: 2,
+      name: "Rifat",
+      address: "Bogura, Sherpur",
+      contactNo: "01639811851",
+      gender: "Male",
+      bloodGroup: "A+",
+      lastDonationDate: "10-12-24",
+    },
+    {
+      id: 3,
+      name: "jayed",
+      address: "Bogura, Sherpur",
+      contactNo: "01639811851",
+      gender: "Male",
+      bloodGroup: "A+",
+      lastDonationDate: "10-12-24",
+    },
   ];
 
   return (
     <div className="container mx-auto mt-10">
       <Search></Search>
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-200">
+        <table className="min-w-full bg-white border border-gray-200 hidden sm:table">
           <thead>
             <tr>
               <th className="py-2 px-4 bg-gray-200 text-left text-gray-600">
@@ -58,6 +76,27 @@ const BloodList = () => {
             ))}
           </tbody>
         </table>
+
+        {/* Mobile view */}
+        <div className="sm:hidden p-2 ">
+          {data.map((item) => (
+            <div key={item.id} className="border p-4 m-8 rounded-lg shadow-md">
+              <div className="flex flex-col">
+                <span className="font-bold">SI:</span> <span>{item.id}</span>
+                <span className="font-bold">Name:</span>{" "}
+                <span>{item.name}</span>
+                <span className="font-bold">Address:</span>{" "}
+                <span>{item.address}</span>
+                <span className="font-bold">Gender:</span>{" "}
+                <span>{item.gender}</span>
+                <span className="font-bold">Blood Group:</span>{" "}
+                <span>{item.bloodGroup}</span>
+                <span className="font-bold">Last Donation Date:</span>{" "}
+                <span>{item.lastDonationDate}</span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
