af4e3afcc44820b96db41768220d53ff653be1ab
Rifat Jayed <rifat.jayed2000@gmail.com>
2025-02-11 19:59:08 +0600

    remove

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -271,26 +271,6 @@ const RegisterPage = () => {
                 </label>
               </div>
             </div>
-            <div className="">
-              <label
-                className=" text-sm font-medium font-roboto text-[#4D4D4D] mb-2 text-left"
-                htmlFor=""
-              >
-                Date Of Birth:
-              </label>
-              <DatePicker
-                required
-                className="w-full px-3 py-2 mb-4 ms-4 border border-gray-300 rounded-md focus:outline-none  "
-                selected={formData.birth}
-                //   onSelect={handleDateSelect}
-                onChange={(date) => {
-                  setFormData({
-                    ...formData,
-                    birth: date,
-                  });
-                }}
-              />
-            </div>
 
             <div>
               <label
