97e2d161bb21efee5ac81c0cf3302938eed49c32
Rifat Jayed <rifat.jayed2000@gmail.com>
2025-02-11 20:34:26 +0600

    env file done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    env({
+      /**
+       * prefix for client-side injection
+       * @default 'VUE_APP'
+       */
+      prefix: "REACT_APP_",
+      /**
+       * client-side env path
+       * @default 'process.env'
+       */
+      mountedPath: "process.env",
+      /**
+       * write to process.env
+       * vite default not，whilte vue-cli yes
+       * @default false
+       */
+      ignoreProcessEnv: true,
+    }),
+  ],
   vite: {
     server: {
       watch: {
